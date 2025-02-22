163d63f33198f50dfe572e0cf7e052cc4f19cae6
Mohsin <sattimohsin838@gmail.com>
2025-02-22 19:03:20 +0500

    "minor changes 2

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,10 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'sattimohsin838@gmail.com',
+    phone: '+923235340053',
+    address: 'I8/3 Islamabad,Pakistan ',
+    github: 'https://github.com/Mohsin-246',
+  facebook: 'https://www.facebook.com/mohsinsatti36/',
+  linkedIn: 'https://www.linkedin.com/in/mohsin-satti/',
+  instagram: 'https://www.instagram.com/mohsinsatti707/',
+
 }

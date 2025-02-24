90931437295e1fb7d380a8ff638b8ec47d43f275
Mohsin <sattimohsin838@gmail.com>
2025-02-24 19:58:36 +0500

    b

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Mohsin Satti",
-  profile: '/mohsin_profile_1.jpg',
+  profile: '@/public/mohsin_profile_1.jpg',
   designation: "Software Developer",
   description: "My name is Mohsin Satti, a passionate and results-driven Full Stack Developer specializing in both web and mobile app development. With a strong foundation in JavaScript, I excel in building scalable, high-performance applications using technologies like React, Next.js, and React Native. I am a quick learner with a self-driven attitude, always eager to explore new technologies and solve complex problems. From crafting seamless web experiences to developing cross-platform mobile applications, I focus on creating efficient, maintainable, and user-friendly solutions. I am always open to exciting job opportunities that align with my skills and interests, aiming to make technology more accessible and impactful.",
   email: 'sattimohsin838@gmail.com',

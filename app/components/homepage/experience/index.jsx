e2dfd6828b737c3efc90d8f3090a4a6c16b974c0
Mohsin <sattimohsin838@gmail.com>
2025-02-26 20:22:20 +0500

    changes

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -6,19 +6,17 @@ import { BsPersonWorkspace } from "react-icons/bs";
 import experience from '../../../assets/lottie/code.json';
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
-import sectionimg from "@/public/section.svg"
-import glow from "@/public/blur-23.svg"
 
 function Experience() {
   return (
     <div id="experience" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
-      <Image
-        src={sectionimg}
+      {/* <Image
+        src="/section.svg"
         alt="Hero"
         width={1572}
         height={795}
         className="absolute top-0 -z-10"
-      />
+      /> */}
 
       <div className="flex justify-center my-5 lg:py-8">
         <div className="flex  items-center">
@@ -44,13 +42,13 @@ function Experience() {
                 experiences.map(experience => (
                   <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
                     <div className="p-3 relative">
-                      <Image
+                      {/* <Image
                         src="/blur-23.svg"
                         alt="Hero"
                         width={1080}
                         height={200}
                         className="absolute bottom-0 opacity-80"
-                      />
+                      /> */}
                       <div className="flex justify-center">
                         <p className="text-xs sm:text-sm text-[#16f2b3]">
                           {experience.duration}
